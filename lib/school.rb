6c70c182780570916dc369aa11126b8d23ceeee4
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-01-29 19:55:46 -0700

    commit tree e8ddc8ea2639301cdc95a2ffae90582bfa1109f4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+    attr_accessor :name, :roster, :student
+  
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+  
+    def add_student(name, student_grade)
+      roster[student_grade] ||= []
+      roster[student_grade] << name
+    end
+  
+    def grade(student_grade)
+      roster[student_grade]
+    end
+  
+    def sort
+      sorted_roster = {}
+      roster.each do |grade, students|
+        sorted_roster[grade] = students.sort
+      end
+      sorted_roster
+    end
+  
+  end
